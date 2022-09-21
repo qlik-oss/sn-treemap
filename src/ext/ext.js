832fe0c495c73d5284cc99d44a59bcb5915184d3
Tobias Linsefors <tobias.linsefors@qlik.com>
2022-09-21 12:47:06 +0200

    feat: add support (same as treemap in sense-client)

--- src/ext/ext.js
+++ src/ext/ext.js
@@ -1,11 +1,26 @@
 import pp from './property-definition';
 
+function hasValidLayout(layout) {
+  return (
+    layout &&
+    !(
+      layout.qHyperCube.qMeasureInfo.every((measure) => measure.qMax <= 0 && measure.qMax >= measure.qMin) ||
+      layout.qHyperCube.qSize.qcy === 0
+    )
+  );
+}
+
 export default function ext(env) {
   return {
     definition: pp(env),
     // TODO soft properties
     support: {
-      // TODO
+      cssScaling: false,
+      snapshot: hasValidLayout,
+      export: hasValidLayout,
+      exportData: true,
+      sharing: hasValidLayout,
+      viewData: true,
     },
     // TODO: object conversion
   };
