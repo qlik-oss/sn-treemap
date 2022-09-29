9893ac2194c7bcede66315a57ccca0e80253a17d
Vittorio Cellucci <vel@qlik.com>
2022-09-29 17:41:50 -0400

    fix: selections

--- src/picasso-def/brushStyles.js
+++ src/picasso-def/brushStyles.js
@@ -1,12 +1,7 @@
-const HIGHLIGHT_STROKE_WIDTH = 2;
 export const active = () => ({
   stroke: 'black',
-  strokeWidth: (d) => {
-    const { select } = d.data;
-    const { source } = d.data;
-    return source?.field === select?.source?.field ? HIGHLIGHT_STROKE_WIDTH : 0;
-  },
-  opacity: (d) => (d?.data?.overlayLabel || d?.data?.parentGroup ? 0 : 1),
+  strokeWidth: (d) => (d.data.child ? 0 : 2),
+  opacity: (d) => (d?.data?.overlay ? 0.7 : 1),
 });
 
 export const inactive = () => ({
---
+++ src/picasso-def/components/defines.js
@@ -0,0 +1,2 @@
+const TREEMAP_DEFINES = Object.freeze({ HEAER_HEIGHT: 28, OUTER_PADDING: 1, STROKE_WIDTH: 0.5 });
+export { TREEMAP_DEFINES };
