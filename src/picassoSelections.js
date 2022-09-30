ddb386346670d9827c1be2cc662dd244412c0947
Vittorio Cellucci <vel@qlik.com>
2022-09-30 08:02:19 -0400

    fix: remove last element on add values

--- src/picassoSelections.js
+++ src/picassoSelections.js
@@ -1,4 +1,25 @@
 export const picassoSelections = ({ selectBrush, picassoQ, selections, lasso }) => {
+  const valueInterceptor = (added) => {
+    const brushes = selectBrush.brushes();
+    brushes.forEach((b) => {
+      if (b.type === 'range') {
+        // has range selections
+        selectBrush.clear([]);
+      }
+    });
+    return added.filter((t) => t.value >= 0).slice(0, -1); // do not allow selection on null or negative value
+  };
+
+  const rangeInterceptor = (a) => {
+    const v = selectBrush.brushes().filter((b) => b.type === 'value');
+    if (v.length) {
+      // has dimension values selected
+      selectBrush.clear([]);
+      return a;
+    }
+    return a;
+  };
+
   if (!lasso) {
     selectBrush.on('update', async (added) => {
       if (!selections.isActive() && added.length > 0) {
@@ -19,4 +40,11 @@ export const picassoSelections = ({ selectBrush, picassoQ, selections, lasso }) 
       }
     });
   }
+
+  selectBrush.intercept('set-ranges', rangeInterceptor);
+  selectBrush.intercept('toggle-ranges', rangeInterceptor);
+
+  selectBrush.intercept('toggle-values', valueInterceptor);
+  selectBrush.intercept('set-values', valueInterceptor);
+  selectBrush.intercept('add-values', valueInterceptor);
 };
