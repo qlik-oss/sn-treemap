ee785d162d7ec80930abeefd82a62b8a1ea56e4d
Tobias Linsefors <tobias.linsefors@qlik.com>
2022-10-04 14:23:09 +0200

    fix: update test

--- src/__tests__/picassodef.test.js
+++ src/__tests__/picassodef.test.js
@@ -9,7 +9,28 @@ describe('Treemap picassodef render', () => {
   it('should render ok', () => {
     const layout = testdata.qLayout[0].value;
     const _theme = themeFn();
-    const data = picassoDef({ layout, theme: _theme.externalAPI, env: {} });
+    const colorService = {
+      getDatumProps: () => ({
+        color: {
+          field: 'qMeasureInfo/0',
+        },
+        expressionColor: undefined,
+        expressionColorText: undefined,
+      }),
+      getScales: () => ({
+        color: {
+          data: {
+            extract: { field: 'qMeasureInfo/0' },
+          },
+          range: ['#ec983d', '#deb357', '#d0ce71', '#c3ea8c', '#8ed195', '#5ab89e', '#26a0a7'],
+          type: 'color',
+        },
+      }),
+      getColor: () => undefined,
+      getLegend: () => undefined,
+      getPalettes: () => undefined,
+    };
+    const data = picassoDef({ layout, theme: _theme.externalAPI, env: {}, colorService });
     expect(data).toMatchSnapshot();
   });
 });
