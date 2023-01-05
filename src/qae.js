b340acbe67f5cba9a69002f07a151e6337fcf91b
Tobias Linsefors <tobias.linsefors@qlik.com>
2023-01-05 11:23:27 +0100

    feat: add dimension and measure descriptions

--- src/qae.js
+++ src/qae.js
@@ -1,13 +1,23 @@
 import customTooltipUtils from './custom-tooltip/utils';
 import objectDefinition from './object-definition';
 
-export const qae = {
+export const qae = ({ translator }) => ({
   data: {
     targets: [
       {
         path: '/qHyperCubeDef',
         dimensions: {
           min: 1,
+          max: 15,
+          description(properties, index) {
+            const dimensionCount = properties.qHyperCubeDef.qDimensions.length;
+            const translationProperty =
+              index >= dimensionCount - 1
+                ? 'Visualizations.Descriptions.Rectangle'
+                : 'Visualizations.Descriptions.Group';
+
+            return translator.get(translationProperty);
+          },
           add(dimension, props) {
             customTooltipUtils.addCallbackCustomTooltip(props);
           },
@@ -24,9 +34,14 @@ export const qae = {
         measures: {
           min: 1,
           max: 1,
+          description(properties, index) {
+            const translationProperty =
+              index === 0 ? 'Visualizations.Descriptions.Size' : 'Visualizations.Descriptions.Color';
+            return translator.get(translationProperty);
+          },
         },
       },
     ],
   },
   properties: objectDefinition(),
-};
+});
