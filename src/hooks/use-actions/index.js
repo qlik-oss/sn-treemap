abb09815334f6165397e4046cce33f0e57fdf1de
Tobias Linsefors <T-Wizard@users.noreply.github.com>
2023-02-20 14:55:58 +0100

    fix: disable legend interaction on constraints.active (#142)

--- src/hooks/use-actions/index.js
+++ src/hooks/use-actions/index.js
@@ -5,6 +5,7 @@ import useSelect from './use-select';
 export default function useActions(options) {
   const constraints = useConstraints();
   const [tooltip, setTooltip] = useState(false);
+  const [interact, setInteract] = useState(false);
   const select = useSelect();
   const lasso = useLasso(options);
 
@@ -14,14 +15,14 @@ export default function useActions(options) {
     }
 
     setTooltip(!constraints.passive);
+    setInteract(!constraints.active);
   }, [constraints]);
 
   // A lot of dependent modules requires an action object. So here we return one in a matching format.
   // reuse and update a simgle object as is stored inside the chart settings and needs to be updated with out calling chart update
   const [actions] = useState({});
-  actions.tooltip = {
-    enabled: () => tooltip,
-  };
+  actions.tooltip = { enabled: () => tooltip };
+  actions.interact = { enabled: () => interact };
   actions.lasso = lasso;
   actions.select = select;
   return actions;
