e5d5cbb00e2e75903b5bf0a6d7744ac9da6c0b88
Tobias Linsefors <tobias.linsefors@qlik.com>
2023-02-23 15:50:35 +0100

    feat: update api properties and mark it public

--- api-spec/props.conf.js
+++ api-spec/props.conf.js
@@ -1,8 +1,19 @@
+const path = require('path');
+
+const pkg = require(path.resolve(__dirname, '../package.json')); // eslint-disable-line
+
 module.exports = {
   fromJsdoc: {
     glob: ['./src/object-definition.js'],
     api: {
       stability: 'stable',
+      properties: {
+        'x-qlik-visibility': 'public',
+      },
+      visibility: 'public',
+      name: `${pkg.name}:properties`,
+      version: pkg.version,
+      description: 'Treemap generic object definition',
     },
     output: {
       sort: {
